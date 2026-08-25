c2ab93c76c38ac9d00e3006d5f9e48c6bb1b114b
Omkar492 <omkarchougule142@gmail.com>
2026-08-25 22:05:36 +0530

    Initial project setup for Match Mate application

---
+++ MatchMate/MatchMate/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MatchMate
+//
+//  Created by Omkar Chougule on 25/08/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
